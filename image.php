b18ccf72cf49bfee9d9385db98f7a1dfecb7e3cc
shivainfo <38890993+shivainfo@users.noreply.github.com>
2018-05-01 14:13:47 -0400

    Update image.php

--- image.php
+++ image.php
@@ -3,8 +3,9 @@
 $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 
-$deployment = getenv("COLOR");
+//$deployment = getenv("COLOR");
 
+$deployment = getenv("green");
 if ( $deployment == 'blue') {
   $color = imagecolorallocate($im, 0, 0, 255);
 } elseif ($deployment == 'green')  {
